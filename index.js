7d7d8ab2735ddd3a0241cc52de53a249392abc41
Jennifer Schlierman <jd9913@gmail.com>
2020-09-26 12:31:06 -0700

    readme format experiments

--- index.js
+++ index.js
@@ -37,6 +37,30 @@ const questions = () => {
         },
         {
             type: 'input',
+            name: 'email',
+            message: "Please enter a contact email address (Required)",
+            validate: contEmail => {
+                if (contEmail) {
+                    return true;
+                } else {
+                    console.log("Please enter a contact email address!")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'repoLink',
+            message: "Please add a link to the repository (Required)",
+            validate: repLink => {
+                if (repLink) {
+                    return true;
+                } else {
+                    console.log("Please enter a contact email address!")
+                }
+            }
+        },
+        {
+            type: 'input',
             name: 'description',
             message: "Please enter the description for your project (Required)",
             validate: fileDesc => {
@@ -95,22 +119,11 @@ const questions = () => {
                 }
             }
         },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Please enter a contact email address (Required)",
-            validate: contEmail => {
-                if (contEmail) {
-                    return true;
-                } else {
-                    console.log("Please enter a contact email address!")
-                }
-            }
-        },
+   
         {
             type: 'checkbox',
             name: 'licenses',
-            choices: ['license1', 'license2', 'license3']
+            choices: ['MIT License', 'GNU GPLv3', 'ApacheLicense 2.0']
         },
 
 
@@ -118,7 +131,41 @@ const questions = () => {
 
 };
 
-questions().then(answers => console.log(answers));
+questions().then(answers => console.log(answers))
+    .then(questions => {
+        promptReadme.projectInfo.push(readmeData);
+        if (promptReadme.confirmAddReadme) {
+            return questions(readmeData);
+        } else {
+            return readmeData;
+        }
+    });
+
+
+const promptReadme = readmeData => {
+
+    //only intialize the variable if it is empty on the first pass
+    if (!readmeData.projectInfo) {
+        readmeData.projectInfo = [];
+    }
+}
+
+const licenseBadge = (licenses) => {
+    const badges = [];
+
+    if (licenses[0]) {
+        badges.push('[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)');
+    };
+
+    if (licenses[1]) {
+        badges.push('[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)');
+    };
+
+    if (licenses[2]) {
+        badges.push('[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)');
+    };
+    return badges;
+};
 
 
 //const readmePage = generateReadme();
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,8 +1,66 @@
 //create the description section
-const generateDescription = descText => {
-	if (!descText) {
-		return '';
-	}
+const generateReadme = (readmeData) => {
+
+    licenseBadge();
+
+    return `
+
+
+${badges}
+
+
+:house:
+#${name}//name of the project
+
+>${description}
+
+
+### Table of Contents
+[Installation instructions](link)<br>
+[Usage Information](link)<br>
+[Contribution Guidelines](link)<br>
+[Testing instructions](link)<br>
+[Questions}(link)<br>
+
+
+:memo:  
+##Installation Instructions
+>${installation}
+
+
+:computer:
+##Usage Information
+>${information}
+
+
+:incoming_envelope:
+##Contribution Guidelines
+>${contributing}
+
+:notebook:
+##Testing instructions
+>${tests}
+
+
+:question:
+##Questions
+>:email:Email: ${email}<br>
+>GitHub Username: ${github}<br>
+>Repository Link: ${repoLink}<br>
+
+
+##Licenses
+>This application is covered under the following license(s): <br>
+>>${licenses}
+>>click [here](https://choosealicense.com/licenses/) to read about these licenses.
+
+####Footer
+
+Made with love :gift_heart: by ${github}. 
+
+
+
+`
 }
 
 
